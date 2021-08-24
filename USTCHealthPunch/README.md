77bf9ebe572dc74370b0014cb19d951982c4f214
cyq <cyq0919@mail.ustc.edu.cn>
2021-08-24 10:28:01 +0800

    add USTCHealthPunch

---
+++ USTCHealthPunch/README.md
@@ -0,0 +1,101 @@
+## 肥科自动健康打卡脚本
+
+**代码介绍见[我的Blog](https://komorebi660.github.io/2021/08/20/USTCHealthPunch/)**
+
+### 安装依赖
+
+安装`OCR`依赖文件：
+
+**Windows**：
+
+访问`https://digi.bib.uni-mannheim.de/tesseract/`，下载`.exe`文件安装.
+
+**Ubuntu**：
+
+```
+sudo apt install tesseract
+```
+
+安装代码所需库：
+
+```
+pip install requests
+pip install Image
+pip install pytesseract
+```
+
+`re`、`io`库为`Python`自带库，无需手动安装。
+
+### 修改参数
+
+接下来，进入源码文件`USTCHealthPunch.py`，修改如下参数(**必选**):
+
+- **USR**：学号
+- **PWD**：密码
+- **LOCATION**：所在校区
+- **EMERGENCY_CONTACT**：紧急联系人姓名
+- **RELATIONSHIP**：本人与紧急联系人关系
+- **PHONE_NUMBER**：紧急联系人电话
+
+然后运行代码，可能会出现以下几种情况：
+
+若出现
+
+```
+上报成功
+```
+
+则表明成功打卡。
+
+若出现
+
+```
+UserName or Password ERROR!
+```
+
+则表明用户名或密码输入错误，或者验证码识别错误，此时可多尝试几次。
+
+若出现
+
+```
+Health Punch ERROR!
+```
+
+则表明上报失败，原因可能是打卡网站进行了更新，需要重新修改代码。
+
+### 上传至`Vlab`定时执行
+
+以`Ubuntu20.04`版本的`Vlab`为例，要实现自动运行脚本需要进行如下设置：
+
+**第一步：上传源码**
+
+将源码上传至`Vlab`虚拟机，你可以使用`ftp`传输。
+
+**第二步：编辑`crontab`**
+
+```
+sudo crontab -e
+```
+
+使用`vim`进行设置，设置格式如下：
+
+```
+* * * * * command
+分 时 日 月 周 命令
+```
+
+例如：
+```
+* 21 * * * /usr/bin/python3 /home/ubuntu/USTCHealthPunc.py
+```
+
+就是每天`21：00`执行`USTCHealthPunch.py`文件
+
+**第三步：重启`cron`服务**
+
+```
+service cron restart
+```
+
+接下来就能解放双手自动打卡了~
+
---
+++ USTCHealthPunch/USTCHealthPunch.py
@@ -0,0 +1,110 @@
+# ----------------------------------------------------------
+# Copyright © 2021 Komorebi660 All rights reserved.
+# ----------------------------------------------------------
+
+import requests
+import re
+from PIL import Image
+from io import BytesIO
+import pytesseract
+from requests.sessions import session
+requests.packages.urllib3.disable_warnings()
+
+EAST = 2  # 东校区
+SOUTH = 3  # 南校区
+MIDDLE = 4  # 中区
+NORTH = 5  # 北区
+WEST = 6  # 西区
+OUT_CAMPUS = 0  # 校外
+
+USR = ''  # 学号
+PWD = ''  # 密码
+LOCATION = WEST  # 所在校区
+EMERGENCY_CONTACT = ''  # 紧急联系人姓名
+RELATIONSHIP = ''  # 本人与紧急联系人关系
+PHONE_NUMBER = ''  # 紧急联系人电话
+
+# 需要在同一session下执行
+session = requests.Session()
+
+
+# --------------------- Step1: 获取CAS_LT ------------------------
+CAS_LT_url = 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin'
+CAS_LT_res = session.get(CAS_LT_url)
+CAS_LT_html = CAS_LT_res.content.decode()
+CAS_LT = re.findall('(?<=name="CAS_LT" value=")(.*?)(?=")', CAS_LT_html)[0]
+# print(CAS_LT)
+# -----------------------------------------------------------------
+
+# ------------------------ Step2: 获取LT --------------------------
+LT_url = 'https://passport.ustc.edu.cn/validatecode.jsp?type=login'
+LT_img = session.get(LT_url).content
+img = Image.open(BytesIO(LT_img))
+# img.show()
+text = pytesseract.image_to_string(img)
+LT = re.sub("\D", "", text)
+# print(LT)
+# -----------------------------------------------------------------
+
+# ----------------------- Step3: 获取token ------------------------
+login_url = 'https://passport.ustc.edu.cn/login'
+login_data = {
+    'model': 'uplogin.jsp',
+    'CAS_LT': CAS_LT,
+    'service': 'https://weixine.ustc.edu.cn/2020/caslogin',
+    'warn': '',
+    'showCode': '1',
+    'username': USR,
+    'password': PWD,
+    'LT': LT,
+    'button': ''
+}
+login_res = session.post(login_url, data=login_data)
+login_html = login_res.content.decode('utf-8')
+token_temp = re.findall('(?<=name="_token" value=")(.*?)(?=")', login_html)
+# 检查返回值是否正确
+if (len(token_temp) == 0):
+    print("UserName or Password ERROR!")
+    exit(0)
+_token = token_temp[0]
+# print(token)
+# -----------------------------------------------------------------
+
+# ------------------------- Step4: 上报 ---------------------------
+post_url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
+post_data = {
+    '_token': _token,
+    'now_address': '1',
+    'gps_now_address': '',
+    'now_province': '340000',
+    'gps_province': '',
+    'now_city': '340100',
+    'gps_city': '',
+    'now_country': '',
+    'gps_country': '',
+    'now_detail': '',
+    'is_inschool': LOCATION,
+    'body_condition': '1',
+    'body_condition_detail': '',
+    'now_status': '1',
+    'now_status_detail': '',
+    'has_fever': '0',
+    'last_touch_sars': '0',
+    'last_touch_sars_date': '',
+    'last_touch_sars_detail': '',
+    'is_danger': '0',
+    'is_goto_danger': '0',
+    'jinji_lxr': EMERGENCY_CONTACT,
+    'jinji_guanxi': RELATIONSHIP,
+    'jiji_mobile': PHONE_NUMBER,
+    'other_detail': ''
+}
+res = session.post(post_url, data=post_data)
+html = res.content.decode('utf-8')
+alert = re.findall('(?<=<p class="alert alert-success">)(.*?)(?=<a)', html)
+if (len(alert) == 0):
+    print("Health Punch ERROR!")
+    exit(0)
+else:
+    print(alert[0])
+# -----------------------------------------------------------------
