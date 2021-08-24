b38e38763c03e288deb5a8bd6c52a1f0cd6dec86
cyq <cyq0919@mail.ustc.edu.cn>
2021-08-24 21:24:41 +0800

    update VerilogTestbenchGen

--- VerilogTestbenchGen/README.md
+++ VerilogTestbenchGen/README.md
@@ -2,6 +2,12 @@
 
 **代码介绍见[我的Blog](https://komorebi660.github.io/2021/08/18/VerilogTestbenchGen/)**
 
+`2021.8.24`更新：
+
+- 生成代码格式优化，更清晰；
+- 报错信息将指明出错行数，快速定位问题；
+- 生成信号部分增添了初始化内容.
+
 ### 代码使用
 
 首先，进入源码`VerilogTestbenchGen.py`，修改以下参数(可选)：
@@ -9,9 +15,21 @@
 - **WRITE_FILE_NAME**：生成结果路径及文件名，默认为`VerilogTestbenchGen/testbench_module.txt`;
 - **READ_FILE_NAME**：待生成`Testbench`的模块的路径及文件名，默认为`VerilogTestbenchGen/module_port.txt`;
 - **TESTBENCH_MODULE_NAME**：`Testbench`模块前缀名，默认为`test_`;
-- **INST_MODULE_NAME**：`Testbench`中例化模块的前缀名，默认为`inst_`.
+- **INST_MODULE_NAME**：`Testbench`中例化模块的前缀名，默认为`inst_`;
 
-接下来，将待生成Testbench的模块接口定义放至`READ_FILE_NAME`对应文件下，注意：使用本代码对输入模块有一定要求，**模块结束的`);`需要单独成行**，如下所示：
+格式字符数目，如接口名称过长，可适当增大以下值：
+
+- **LINE_LENTH_1**：默认为`20`;
+- **LINE_LENTH_2**：默认为`15`;
+- **LINE_LENTH_3**：默认为`30`;
+
+接下来，将待生成`Testbench`的模块接口定义放至`READ_FILE_NAME`对应文件下，注意：使用本代码对输入模块有一定要求:
+
+- `(`后不能出现字符；
+- 模块结束的`);`需要单独成行；
+- 不支持带参数的模块；
+
+一个恰当的格式如下：
 
 ```v
 module module_name(
@@ -23,7 +41,7 @@ module module_name(
 );
 ```
 
-然后运行代码，结果会出现在`WRITE_FILE_NAME`对应文件中.
+将模块以合适的格式放至正确的文件中后，运行代码，结果会出现在`WRITE_FILE_NAME`对应文件中.
 
 ### 测试样例及结果
 
@@ -85,83 +103,102 @@ Testbench file made by VerilogTestbenchGen.py
 
 module test_AXI_Top();
 
-reg  clk;
-reg  rstn;
-reg  i_rd_req;
-reg  [2:0] i_rd_type;
-reg  [31:0] i_rd_addr;
-wire i_rd_rdy;
-wire i_ret_valid;
-wire i_ret_last;
-wire [31:0] i_ret_data;
-reg  i_wr_req;
-reg  [2:0] i_wr_type;
-reg  [31:0] i_wr_addr;
-reg  [3:0] i_wr_wstrb;
-reg  [127:0] i_wr_data;
-wire i_wr_rdy;
-reg  d_rd_req;
-reg  [2:0] d_rd_type;
-reg  [31:0] d_rd_addr;
-wire d_rd_rdy;
-wire d_ret_valid;
-wire d_ret_last;
-wire [31:0] d_ret_data;
-reg  d_wr_req;
-reg  [2:0] d_wr_type;
-reg  [31:0] d_wr_addr;
-reg  [3:0] d_wr_wstrb;
-reg  [127:0] d_wr_data;
-wire d_wr_rdy;
-wire TxD;
-reg  RxD;
-wire interrupt;
-wire [11:0] vrgb;
-wire hs;
-wire vs;
+reg                 clk;
+reg                 rstn;
+reg                 i_rd_req;
+reg  [2:0]          i_rd_type;
+reg  [31:0]         i_rd_addr;
+wire                i_rd_rdy;
+wire                i_ret_valid;
+wire                i_ret_last;
+wire [31:0]         i_ret_data;
+reg                 i_wr_req;
+reg  [2:0]          i_wr_type;
+reg  [31:0]         i_wr_addr;
+reg  [3:0]          i_wr_wstrb;
+reg  [127:0]        i_wr_data;
+wire                i_wr_rdy;
+reg                 d_rd_req;
+reg  [2:0]          d_rd_type;
+reg  [31:0]         d_rd_addr;
+wire                d_rd_rdy;
+wire                d_ret_valid;
+wire                d_ret_last;
+wire [31:0]         d_ret_data;
+reg                 d_wr_req;
+reg  [2:0]          d_wr_type;
+reg  [31:0]         d_wr_addr;
+reg  [3:0]          d_wr_wstrb;
+reg  [127:0]        d_wr_data;
+wire                d_wr_rdy;
+wire                TxD;
+reg                 RxD;
+wire                interrupt;
+wire [11:0]         vrgb;
+wire                hs;
+wire                vs;
+
 
 initial
 begin
-	
+	clk           ='d0;
+	rstn          ='d0;
+	i_rd_req      ='d0;
+	i_rd_type     ='d0;
+	i_rd_addr     ='d0;
+	i_wr_req      ='d0;
+	i_wr_type     ='d0;
+	i_wr_addr     ='d0;
+	i_wr_wstrb    ='d0;
+	i_wr_data     ='d0;
+	d_rd_req      ='d0;
+	d_rd_type     ='d0;
+	d_rd_addr     ='d0;
+	d_wr_req      ='d0;
+	d_wr_type     ='d0;
+	d_wr_addr     ='d0;
+	d_wr_wstrb    ='d0;
+	d_wr_data     ='d0;
+	RxD           ='d0;
 end
 
 
 AXI_Top inst_AXI_Top
 (
-    .clk(clk),
-    .rstn(rstn),
-    .i_rd_req(i_rd_req),
-    .i_rd_type(i_rd_type),
-    .i_rd_addr(i_rd_addr),
-    .i_rd_rdy(i_rd_rdy),
-    .i_ret_valid(i_ret_valid),
-    .i_ret_last(i_ret_last),
-    .i_ret_data(i_ret_data),
-    .i_wr_req(i_wr_req),
-    .i_wr_type(i_wr_type),
-    .i_wr_addr(i_wr_addr),
-    .i_wr_wstrb(i_wr_wstrb),
-    .i_wr_data(i_wr_data),
-    .i_wr_rdy(i_wr_rdy),
-    .d_rd_req(d_rd_req),
-    .d_rd_type(d_rd_type),
-    .d_rd_addr(d_rd_addr),
-    .d_rd_rdy(d_rd_rdy),
-    .d_ret_valid(d_ret_valid),
-    .d_ret_last(d_ret_last),
-    .d_ret_data(d_ret_data),
-    .d_wr_req(d_wr_req),
-    .d_wr_type(d_wr_type),
-    .d_wr_addr(d_wr_addr),
-    .d_wr_wstrb(d_wr_wstrb),
-    .d_wr_data(d_wr_data),
-    .d_wr_rdy(d_wr_rdy),
-    .TxD(TxD),
-    .RxD(RxD),
-    .interrupt(interrupt),
-    .vrgb(vrgb),
-    .hs(hs),
-    .vs(vs)
+	.clk(clk),                   // input
+	.rstn(rstn),                 // input
+	.i_rd_req(i_rd_req),         // input
+	.i_rd_type(i_rd_type),       // input [2:0]
+	.i_rd_addr(i_rd_addr),       // input [31:0]
+	.i_rd_rdy(i_rd_rdy),         // output
+	.i_ret_valid(i_ret_valid),   // output
+	.i_ret_last(i_ret_last),     // output
+	.i_ret_data(i_ret_data),     // output [31:0]
+	.i_wr_req(i_wr_req),         // input
+	.i_wr_type(i_wr_type),       // input [2:0]
+	.i_wr_addr(i_wr_addr),       // input [31:0]
+	.i_wr_wstrb(i_wr_wstrb),     // input [3:0]
+	.i_wr_data(i_wr_data),       // input [127:0]
+	.i_wr_rdy(i_wr_rdy),         // output
+	.d_rd_req(d_rd_req),         // input
+	.d_rd_type(d_rd_type),       // input [2:0]
+	.d_rd_addr(d_rd_addr),       // input [31:0]
+	.d_rd_rdy(d_rd_rdy),         // output
+	.d_ret_valid(d_ret_valid),   // output
+	.d_ret_last(d_ret_last),     // output
+	.d_ret_data(d_ret_data),     // output [31:0]
+	.d_wr_req(d_wr_req),         // input
+	.d_wr_type(d_wr_type),       // input [2:0]
+	.d_wr_addr(d_wr_addr),       // input [31:0]
+	.d_wr_wstrb(d_wr_wstrb),     // input [3:0]
+	.d_wr_data(d_wr_data),       // input [127:0]
+	.d_wr_rdy(d_wr_rdy),         // output
+	.TxD(TxD),                   // output
+	.RxD(RxD),                   // input
+	.interrupt(interrupt),       // output
+	.vrgb(vrgb),                 // output [11:0]
+	.hs(hs),                     // output
+	.vs(vs)                      // output
 );
 
 endmodule
--- VerilogTestbenchGen/VerilogTestbenchGen.py
+++ VerilogTestbenchGen/VerilogTestbenchGen.py
@@ -7,6 +7,10 @@ READ_FILE_NAME = 'VerilogTestbenchGen/module_port.txt'
 TESTBENCH_MODULE_NAME = 'test_'
 INST_MODULE_NAME = 'inst_'
 
+LINE_LENTH_1 = 20
+LINE_LENTH_2 = 15
+LINE_LENTH_3 = 30
+
 f_out = open(WRITE_FILE_NAME, 'w+')
 f_out.write('`timescale 1ns / 1ps \n')
 f_out.write('/*------------------------------------------------\n')
@@ -14,66 +18,96 @@ f_out.write('Testbench file made by VerilogTestbenchGen.py\n')
 f_out.write('------------------------------------------------*/\n\n\n')
 
 # generate ports
+start = 0
+line_number = 0
 with open(READ_FILE_NAME, 'r') as f:
     while True:
         line_input = f.readline()
+        line_number += 1
         line_temp = line_input.split()
+        lenth = len(line_temp)
         # read a empty line doesn't mean the module defination is over.
-        if (len(line_temp) == 0):
+        if lenth == 0:
             continue
         # module is over.
         elif line_temp[0] == ');':
             break
 
+        elif line_temp[0] == 'module(':
+            print(f'Can not find module name in line {line_number}.')
+            exit(0)
         elif line_temp[0] == 'module':
-            if len(line_temp) < 2:
-                print("Can not find module name!")
-                exit(0)
+            # the start of a module
+            start = 1
             # delete '(' in the module name
             module_name = line_temp[1].replace('(', '')
+            # input module error
+            if len(module_name) == 0:
+                print(f'Can not find module name in line {line_number}.')
+                exit(0)
             line_output = 'module '
             line_output += TESTBENCH_MODULE_NAME+module_name
             line_output += '();\n'
             f_out.write(line_output)
 
         elif line_temp[0] == 'input':
+            if start == 0:
+                continue
             line_output = 'reg '
-            if len(line_temp) < 2:
-                print("Can not find input port name!")
+            if lenth < 2:
+                print(f"Can not find input port name in line {line_number}.")
                 exit(0)
-            for i in range(1, len(line_temp)):
+            elif lenth == 2 and line_temp[1] == ',':
+                print(f"Can not find output port name in line {line_number}.")
+                exit(0)
+            for i in range(1, lenth):
                 # ignore 'wire' or 'reg'
                 if line_temp[i] == 'wire':
                     continue
                 elif line_temp[i] == 'reg':
                     continue
+                # if the last word is ','
+                elif i == lenth-2 and line_temp[lenth-1] == ',':
+                    line_output += ' '*(LINE_LENTH_1-len(line_output))
+                    line_output += line_temp[i]+';'
+                    break
+                # if it is the last word
+                elif i == lenth-1:
+                    line_output += ' '*(LINE_LENTH_1-len(line_output))
+                    # there may not have a ',' in the last word
+                    line_output += line_temp[i].replace(',', '')+';'
                 else:
-                    # replace ',' to ';' but in case that the last port in the module doesn't have a ','
-                    if i == len(line_temp)-1:
-                        line_output += ' '+line_temp[i].replace(',', '')
-                        line_output += ';'
-                    else:
-                        line_output += ' '+line_temp[i]
+                    line_output += ' '+line_temp[i]
             f_out.write('\n'+line_output)
 
         elif line_temp[0] == 'output':
+            if start == 0:
+                continue
             line_output = 'wire'
-            if len(line_temp) < 2:
-                print("Can not find output port name!")
+            if lenth < 2:
+                print(f"Can not find output port name in line {line_number}.")
                 exit(0)
-            for i in range(1, len(line_temp)):
+            elif lenth == 2 and line_temp[1] == ',':
+                print(f"Can not find output port name in line {line_number}.")
+                exit(0)
+            for i in range(1, lenth):
                 # ignore 'wire' or 'reg'
                 if line_temp[i] == 'wire':
                     continue
                 elif line_temp[i] == 'reg':
                     continue
+                # if the last word is ','
+                elif i == lenth-2 and line_temp[lenth-1] == ',':
+                    line_output += ' '*(LINE_LENTH_1-len(line_output))
+                    line_output += line_temp[i]+';'
+                    break
+                # if it is the last word
+                elif i == lenth-1:
+                    line_output += ' '*(LINE_LENTH_1-len(line_output))
+                    # there may not have a ',' in the last word
+                    line_output += line_temp[i].replace(',', '')+';'
                 else:
-                    # replace ',' to ';' but in case that the last port in the module doesn't have a ','
-                    if i == len(line_temp)-1:
-                        line_output += ' '+line_temp[i].replace(',', '')
-                        line_output += ';'
-                    else:
-                        line_output += ' '+line_temp[i]
+                    line_output += ' '+line_temp[i]
             f_out.write('\n'+line_output)
 
         else:
@@ -81,28 +115,61 @@ with open(READ_FILE_NAME, 'r') as f:
 f.close()
 
 # generate signals
-f_out.write('\n\ninitial\n')
-f_out.write('begin\n')
-f_out.write('\t\n')
-f_out.write('end\n')
-
-# instant module
+f_out.write('\n\n\ninitial\n')
+f_out.write('begin')
+start = 0
 with open(READ_FILE_NAME, 'r') as f:
     while True:
         line_input = f.readline()
         line_temp = line_input.split()
+        lenth = len(line_temp)
         # read a empty line doesn't mean the module defination is over.
-        if (len(line_temp) == 0):
+        if lenth == 0:
+            continue
+        # module is over.
+        if line_temp[0] == ');':
+            break
+        # the start of a module
+        elif line_temp[0] == 'module':
+            start = 1
+        # generate input signal
+        elif line_temp[0] == 'input':
+            if start == 0:
+                continue
+            # get the last word
+            line_output = line_temp[lenth-1]
+            # if the last word is ','
+            if line_output == ',':
+                line_output = '\t'+line_temp[lenth-2]
+                line_output += ' '*(LINE_LENTH_2-len(line_output))
+                line_output = line_output+'=\'d0;'
+            else:
+                line_output = '\t'+line_output.replace(',', '')
+                line_output += ' '*(LINE_LENTH_2-len(line_output))
+                line_output = line_output+'=\'d0;'
+            f_out.write('\n'+line_output)
+        else:
+            continue
+f.close()
+f_out.write('\nend\n')
+
+# instant module
+start = 0
+with open(READ_FILE_NAME, 'r') as f:
+    while True:
+        line_input = f.readline()
+        line_temp = line_input.split()
+        lenth = len(line_temp)
+        # read a empty line doesn't mean the module defination is over.
+        if (lenth == 0):
             continue
         # module is over.
         elif line_temp[0] == ');':
             f_out.write('\n);')
             break
-
+        # the start of a module
         elif line_temp[0] == 'module':
-            if len(line_temp) < 2:
-                print("Can not find module name!")
-                exit(0)
+            start = 1
             # delete '(' in the module name
             module_name = line_temp[1].replace('(', '')
             line_output = module_name+" "
@@ -111,20 +178,35 @@ with open(READ_FILE_NAME, 'r') as f:
             f_out.write('\n\n'+line_output)
 
         elif line_temp[0] == 'input' or line_temp[0] == 'output':
-            if len(line_temp) < 2:
-                print("Can not find port name!")
-                exit(0)
+            if start == 0:
+                continue
             # get the last word
-            line_output = line_temp[len(line_temp)-1]
+            line_output = line_temp[lenth-1]
             # if the last word is ','
             if line_output == ',':
-                line_output = line_temp[len(line_temp)-2]
+                line_output = line_temp[lenth-2]
+                line_output = '\t.'+line_output+'('+line_output+'),'
             # if the last letter of the last word is ','
             elif line_output[len(line_output)-1] == ',':
                 line_output = line_output.replace(',', '')
                 line_output = '\t.'+line_output+'('+line_output+'),'
             else:
                 line_output = '\t.'+line_output+'('+line_output+')'
+            # add scripts of the ports
+            line_output += ' '*(LINE_LENTH_3-len(line_output))
+            line_output += '//'
+            for i in range(0, lenth-1):
+                # ignore 'wire' or 'reg'
+                if line_temp[i] == 'wire':
+                    continue
+                elif line_temp[i] == 'reg':
+                    continue
+                else:
+                    # if the last word is ','
+                    if i == lenth-2 and line_temp[lenth-1] == ',':
+                        continue
+                    else:
+                        line_output += ' '+line_temp[i]
             f_out.write('\n'+line_output)
 
         else:
